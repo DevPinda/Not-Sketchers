7891275f98007bde5bff9e8ee3d2984668c8fdb2
Kazi Arham <210149902@aston.ac.uk>
2023-03-01 17:41:22 +0000

    Making JSON and local storage to add product

--- Not-Sketchers/src/main/resources/static/js/products.js
+++ Not-Sketchers/src/main/resources/static/js/products.js
@@ -7,9 +7,42 @@ window.onload = function () {
     basket_btn = document.getElementById('basket-button');
     
     var basketItemsAmount = 0;
+    var products_list = []
 
+    // Functionality to click the 'Add To Basket' button from the products page
     basket_btn.addEventListener("click", function () {
         basketItemsAmount += 1;
         console.log(basketItemsAmount);
+
+        // Using JSON and local storage to store the product in local storage
+        if (typeof(Storage) !== 'undefined') {
+            let shoe_item = {
+                id: 1,
+                title: product_name,
+                gender_style: gender,
+                shoe_colour: colour,
+                size: shoe_size,
+                price: product_price,
+                number_of_items: 1
+            };
+
+            if (JSON.parse(localStorage.getItem("shoe_items")) === null) {
+                products_list.push(shoe_item);
+                localStorage.setItem("shoe_items", JSON.stringify(products_list));
+                window.location.reload();
+            } else {
+                const local_items = JSON.parse(localStorage.getItem("shoe_items"));
+                local_items.map(data => {
+                    if (shoe_item.id == data.id) {
+                        shoe_item.number_of_items = data.number_of_items + 1;
+                    } else {
+                        products_list.push(data);
+                    }
+                });
+                products_list.push(shoe_item);
+                localStorage.setItem("shoe_items", JSON.stringify(products_list));
+                window.location.reload();
+            }
+        } 
     });
 }
