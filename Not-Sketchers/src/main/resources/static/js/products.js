67fd24550f064834473356f43f979c07194cb044
Kazi Arham <210149902@aston.ac.uk>
2023-03-02 10:12:21 +0000

    Testing the local storage and JSON to add product

--- Not-Sketchers/src/main/resources/static/js/products.js
+++ Not-Sketchers/src/main/resources/static/js/products.js
@@ -10,7 +10,7 @@ window.onload = function () {
     var products_list = []
 
     // Functionality to click the 'Add To Basket' button from the products page
-    basket_btn.addEventListener("click", function () {
+    basket_btn.addEventListener("click", function (e) {
         basketItemsAmount += 1;
         console.log(basketItemsAmount);
 
