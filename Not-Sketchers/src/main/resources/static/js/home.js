473913d01938681b4390f89be2e8af20eadadc69
rabiafatima1 <115074530+rabiafatima1@users.noreply.github.com>
2023-03-24 14:01:58 +0000

    java script for home page button
    need help with event listener

---
+++ Not-Sketchers/src/main/resources/static/js/home.js
@@ -0,0 +1,6 @@
+// get men and women buttons and add event listener
+const menbutton= document.getElementById("menb");
+menbutton.addEventListener();
+const  womenbutton= document.getElementById("womenb");
+menbutton.addEventListener();
+
